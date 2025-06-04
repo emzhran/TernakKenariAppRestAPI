1180ed9d9ee31f9033645e1b818a701d715ad5ce
emzhran <zachran750@gmail.com>
2025-06-04 21:44:18 +0700

    membuat profile buyer repository untuk mengelola request dan response method addProfileBuyer

---
+++ lib/data/repository/profile_buyer_repository.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:canarytemplate/data/model/request/buyer/buyer_profile_request_model.dart';
+import 'package:canarytemplate/data/model/response/buyer/buyer_profile_response_model.dart';
+import 'package:canarytemplate/service/service_http_client.dart';
+import 'package:dartz/dartz.dart';
+
+class ProfileBuyerRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  ProfileBuyerRepository(this._serviceHttpClient);
+
+  Future<Either<String, BuyerProfileResponseModel>> addProfileBuyer(
+    Buyerprofilerequestmodel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "buyer/profile",
+        requestModel.toMap(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = BuyerProfileResponseModel.fromJson(
+          jsonResponse,
+        );
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? "Unknown error occurred");
+      }
+    } catch (e) {
+      return Left("An error occurred while adding profile: $e");
+    }
+  }
+}
