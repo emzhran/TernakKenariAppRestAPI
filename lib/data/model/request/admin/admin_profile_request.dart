32e783e8f7e86c8400652adda9e4085fe107c22d
emzhran <zachran750@gmail.com>
2025-06-04 19:01:16 +0700

    perbaikan pada model admin, anak, dan induk request

--- lib/data/model/request/admin/admin_profile_request.dart
+++ lib/data/model/request/admin/admin_profile_request.dart
@@ -1,14 +1,10 @@
 import 'dart:convert';
 
 class Adminprofilerequest {
-    final String? message;
-    final int? statusCode;
-    final Data? data;
+    final String? name;
 
     Adminprofilerequest({
-        this.message,
-        this.statusCode,
-        this.data,
+        this.name,
     });
 
     factory Adminprofilerequest.fromJson(String str) => Adminprofilerequest.fromMap(json.decode(str));
@@ -16,38 +12,10 @@ class Adminprofilerequest {
     String toJson() => json.encode(toMap());
 
     factory Adminprofilerequest.fromMap(Map<String, dynamic> json) => Adminprofilerequest(
-        message: json["message"],
-        statusCode: json["status_code"],
-        data: json["data"] == null ? null : Data.fromMap(json["data"]),
-    );
-
-    Map<String, dynamic> toMap() => {
-        "message": message,
-        "status_code": statusCode,
-        "data": data?.toMap(),
-    };
-}
-
-class Data {
-    final int? id;
-    final String? name;
-
-    Data({
-        this.id,
-        this.name,
-    });
-
-    factory Data.fromJson(String str) => Data.fromMap(json.decode(str));
-
-    String toJson() => json.encode(toMap());
-
-    factory Data.fromMap(Map<String, dynamic> json) => Data(
-        id: json["id"],
         name: json["name"],
     );
 
     Map<String, dynamic> toMap() => {
-        "id": id,
         "name": name,
     };
 }
--- lib/data/model/request/admin/anak_request_model.dart
+++ lib/data/model/request/admin/anak_request_model.dart
@@ -1,14 +1,22 @@
 import 'dart:convert';
 
 class Anakrequestmodel {
-    final String? message;
-    final int? statusCode;
-    final Data? data;
+    final String? noRing;
+    final DateTime? tanggalLahir;
+    final String? jenisKelamin;
+    final String? jenisKenari;
+    final int? ayahId;
+    final int? ibuId;
+    final dynamic gambarAnak;
 
     Anakrequestmodel({
-        this.message,
-        this.statusCode,
-        this.data,
+        this.noRing,
+        this.tanggalLahir,
+        this.jenisKelamin,
+        this.jenisKenari,
+        this.ayahId,
+        this.ibuId,
+        this.gambarAnak,
     });
 
     factory Anakrequestmodel.fromJson(String str) => Anakrequestmodel.fromMap(json.decode(str));
@@ -16,62 +24,22 @@ class Anakrequestmodel {
     String toJson() => json.encode(toMap());
 
     factory Anakrequestmodel.fromMap(Map<String, dynamic> json) => Anakrequestmodel(
-        message: json["message"],
-        statusCode: json["status_code"],
-        data: json["data"] == null ? null : Data.fromMap(json["data"]),
-    );
-
-    Map<String, dynamic> toMap() => {
-        "message": message,
-        "status_code": statusCode,
-        "data": data?.toMap(),
-    };
-}
-
-class Data {
-    final int? id;
-    final String? noRing;
-    final DateTime? tanggalLahir;
-    final String? jenisKelamin;
-    final String? jenisKenari;
-    final dynamic gambarAnak;
-    final String? ayahNoRing;
-    final String? ibuNoRing;
-
-    Data({
-        this.id,
-        this.noRing,
-        this.tanggalLahir,
-        this.jenisKelamin,
-        this.jenisKenari,
-        this.gambarAnak,
-        this.ayahNoRing,
-        this.ibuNoRing,
-    });
-
-    factory Data.fromJson(String str) => Data.fromMap(json.decode(str));
-
-    String toJson() => json.encode(toMap());
-
-    factory Data.fromMap(Map<String, dynamic> json) => Data(
-        id: json["id"],
         noRing: json["no_ring"],
         tanggalLahir: json["tanggal_lahir"] == null ? null : DateTime.parse(json["tanggal_lahir"]),
         jenisKelamin: json["jenis_kelamin"],
         jenisKenari: json["jenis_kenari"],
+        ayahId: json["ayah_id"],
+        ibuId: json["ibu_id"],
         gambarAnak: json["gambar_anak"],
-        ayahNoRing: json["ayah_no_ring"],
-        ibuNoRing: json["ibu_no_ring"],
     );
 
     Map<String, dynamic> toMap() => {
-        "id": id,
         "no_ring": noRing,
         "tanggal_lahir": "${tanggalLahir!.year.toString().padLeft(4, '0')}-${tanggalLahir!.month.toString().padLeft(2, '0')}-${tanggalLahir!.day.toString().padLeft(2, '0')}",
         "jenis_kelamin": jenisKelamin,
         "jenis_kenari": jenisKenari,
+        "ayah_id": ayahId,
+        "ibu_id": ibuId,
         "gambar_anak": gambarAnak,
-        "ayah_no_ring": ayahNoRing,
-        "ibu_no_ring": ibuNoRing,
     };
 }
