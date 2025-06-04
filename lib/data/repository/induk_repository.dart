8e1408d413eaaa5460cfca2df540bb460c4e35ab
emzhran <zachran750@gmail.com>
2025-06-04 20:39:13 +0700

    membuat induk repository untuk mengelola request dan response method addinduk

---
+++ lib/data/repository/induk_repository.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:canarytemplate/data/model/request/admin/induk_request_model.dart';
+import 'package:canarytemplate/data/model/response/get_all_induk_response.dart';
+import 'package:canarytemplate/service/service_http_client.dart';
+import 'package:dartz/dartz.dart';
+
+class IndukRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  IndukRepository(this._serviceHttpClient);
+
+  Future<Either<String, GetIndukById>> addInduk(
+    Indukrequestmodel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "admin/induk",
+        requestModel.toMap(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileRespnose = GetIndukById.fromJson(jsonResponse);
+        return Right(profileRespnose);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? "Unknown error occurred");
+      }
+    } catch (e) {
+      return Left("An error occurred while adding induk: $e");
+    }
+  }
+}
