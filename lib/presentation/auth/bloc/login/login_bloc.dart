a234d91df8844e69985382b768a049d3b50e7f71
emzhran <zachran750@gmail.com>
2025-06-05 17:04:10 +0700

    membuat login bloc untuk mengelola event dan state login pengguna

---
+++ lib/presentation/auth/bloc/login/login_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:canarytemplate/data/repository/auth_repository.dart';
+import 'package:canarytemplate/presentation/auth/bloc/login/login_event.dart';
+import 'package:canarytemplate/presentation/auth/bloc/login/login_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository authRepository;
+
+  LoginBloc({required this.authRepository}) : super(LoginInitial()) {
+    on<LoginRequested>(_onLoginRequested);
+  }
+
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<LoginState> emit,
+  ) async {
+    emit(LoginLoading());
+
+    final result = await authRepository.login(event.requestModel);
+
+    result.fold(
+      (l) => emit(LoginFailure(error: l)),
+      (r) => emit(LoginSuccess(responseModel: r)),
+    );
+  }
+}
