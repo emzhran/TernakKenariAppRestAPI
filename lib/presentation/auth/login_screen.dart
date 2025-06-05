4a98ee806026e95736434409292eb142835ad58a
emzhran <zachran750@gmail.com>
2025-06-05 18:16:27 +0700

    menambahkan text field untuk input email

--- lib/presentation/auth/login_screen.dart
+++ lib/presentation/auth/login_screen.dart
@@ -1,3 +1,6 @@
+import 'package:canarytemplate/core/components/custom_text_field.dart';
+import 'package:canarytemplate/core/components/spaces.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -31,6 +34,37 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SpaceHeight(170),
+              Text(
+                'SELAMAT DATANG KEMBALI',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SpaceHeight(30),
+              CustomTextField(
+                validator: 'Email tidak boleh kosong',
+                controller: emailController, 
+                label: 'Email',
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.email),
+                ), 
+              )
+            ],
+          ),
+          ),
+        ),
+      ),
+    );
   }
 }
