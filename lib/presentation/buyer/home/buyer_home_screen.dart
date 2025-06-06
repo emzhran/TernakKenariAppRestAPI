c712bb666704676ebfc4ff55fed221dcbec95d60
emzhran <zachran750@gmail.com>
2025-06-06 22:24:51 +0700

    menambahkan appbar dan tombol logout serta dialog konfirmasi

--- lib/presentation/buyer/home/buyer_home_screen.dart
+++ lib/presentation/buyer/home/buyer_home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:canarytemplate/presentation/auth/login_screen.dart';
 import 'package:canarytemplate/presentation/bloc/get_all_burung_tersedia/bloc/getburungtersedia_bloc.dart';
 import 'package:canarytemplate/presentation/bloc/get_all_burung_tersedia/bloc/getburungtersedia_event.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,6 +21,42 @@ class _BuyerHomeScreenState extends State<BuyerHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.black),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return CupertinoAlertDialog(
+                    title: const Text('Konfirmasi'),
+                    content: const Text('Apakah anda yakin ingin keluar?'),
+                    actions: [
+                      CupertinoDialogAction(
+                        child: const Text('Batal'),
+                        onPressed: () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                            (route) => false,
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
